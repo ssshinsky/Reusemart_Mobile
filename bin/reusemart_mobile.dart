e6afcd15c786b00cf20adf22d3478829264a744f
ssshinsky <220711884@students.uajy.ac.id>
2025-05-31 13:30:38 +0700

    first commit

---
+++ bin/reusemart_mobile.dart
@@ -0,0 +1,5 @@
+import 'package:reusemart_mobile/reusemart_mobile.dart' as reusemart_mobile;
+
+void main(List<String> arguments) {
+  print('Hello world: ${reusemart_mobile.calculate()}!');
+}
---
+++ test/reusemart_mobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:reusemart_mobile/reusemart_mobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
